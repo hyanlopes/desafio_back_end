f563ce0d494f97cf12b2d43a1227372df2a29b26
Hyan Lopes <hyanlopess97@gmail.com>
2022-10-09 13:52:44 -0300

    feat: list by store maked with the correct format

--- store/serializers.py
+++ store/serializers.py
@@ -1,4 +1,6 @@
-from rest_framework.serializers import ModelSerializer
+from management.models import Management
+from management.serializers import ManagementSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from .models import Store
 
@@ -7,3 +9,32 @@ class StoreSerializer(ModelSerializer):
     class Meta:
         model = Store
         fields = "__all__"
+
+
+class StoreListSerializer(ModelSerializer):
+    Management = ManagementSerializer(many=True)
+
+    class Meta:
+        model = Store
+        fields = "__all__"
+
+    sub_total = SerializerMethodField()
+
+    def get_sub_total(self, management):
+        sub_total = 0
+        transactions = Management.objects.filter(store_id=management.id)
+        for i in transactions:
+
+            if (
+                i.type == "1"
+                or i.type == "4"
+                or i.type == "5"
+                or i.type == "6"
+                or i.type == "7"
+                or i.type == "8"
+            ):
+
+                sub_total = sub_total + i.value
+            else:
+                sub_total = sub_total - i.value
+        return sub_total
--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,6 @@ from . import views as view
 
 urlpatterns = [
     path("store/", view.StoreView.as_view()),
+    path("store/<str:name>/", view.StoreListView.as_view()),
     path("login/", views.obtain_auth_token, name="login"),
 ]
