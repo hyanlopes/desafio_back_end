0b4846874e362ef954276f00c46092a94aaa87fd
Hyan Lopes <hyanlopess97@gmail.com>
2022-10-10 17:26:48 -0300

    feat: description for tests created

--- store/tests/test_model.py
+++ store/tests/test_model.py
@@ -8,10 +8,20 @@ class TestsModel(APITestCase):
         self.store = Store.objects.create(**store_data)
 
     def test_store_name_max_length(self):
+
+        """
+        Test to verify the max_length of the store name is correct
+        """
+
         max_length = self.store._meta.get_field("name").max_length
         self.assertEqual(max_length, 30)
 
     def test_store_id_type(self):
+
+        """
+        Test to verify if the id type is equal to uuidField
+        """
+
         type = self.store._meta.get_field("id")._description()
 
         self.assertEqual(type, "Field of type: UUIDField")
--- store/tests/test_view.py
+++ store/tests/test_view.py
@@ -9,14 +9,29 @@ class TestsView(APITestCase):
         cls.store_data = {"name": "Test store"}
 
     def test_store_created_with_success(self):
+
+        """
+        Test to verify if the store will be created with success using the route
+        """
+
         response = self.client.post(self.base_url, data=self.store_data)
         self.assertEqual(201, response.status_code)
 
     def test_list_all_store(self):
+
+        """
+        Test to verify if all stores regitered will be returned
+        """
+
         response = self.client.get(self.base_url)
         self.assertListEqual(response.data, [])
 
     def test_list_especific_store(self):
+
+        """
+        Test to verify if a unique store by store_name will be returned
+        """
+
         self.client.post(self.base_url, self.store_data)
         response = self.client.get(f"/api/store/{self.store_data['name']}/")
         self.assertEqual(200, response.status_code)
