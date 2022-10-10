961c7704dde45c062f050fc6bb95f36b66fd9ecc
Hyan Lopes <hyanlopess97@gmail.com>
2022-10-10 14:15:52 -0300

    feat: test for listing of store maked

--- store/tests/test_view.py
+++ store/tests/test_view.py
@@ -1,6 +1,5 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
-from store.models import Store
 
 
 class TestsView(APITestCase):
@@ -12,3 +11,12 @@ class TestsView(APITestCase):
     def test_store_created_with_success(self):
         response = self.client.post(self.base_url, data=self.store_data)
         self.assertEqual(201, response.status_code)
+
+    def test_list_all_store(self):
+        response = self.client.get(self.base_url)
+        self.assertListEqual(response.data, [])
+
+    def test_list_especific_store(self):
+        self.client.post(self.base_url, self.store_data)
+        response = self.client.get(f"/api/store/{self.store_data['name']}/")
+        self.assertEqual(200, response.status_code)
